3aff379d5ef235fd1873da6948b3033ad1727576
Ivan Escobar <ivnescbr1989@gmail.com>
2023-09-14 16:48:35 -0400

    single user request logic

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -2,8 +2,21 @@ import { user } from "../models/user";
 import {Request, Response} from 'express'
 
 module.exports = {
-    async getUser(){
-
+    async getUser(req: Request, res: Response){
+        try {
+        const activeUser = await user.findOne(
+            {
+                username: req.params.username
+            }
+        )
+        return !activeUser 
+                ? res.status(400).json({ message: 'Cannot find a user with this username' })
+                : res.status(200).json(activeUser)
+            
+        } catch(error) {
+            console.error(error)
+            return res.status(500).json({ message: 'server error, skill issue likely ;)' });
+        }
     },
     async editUser() { 
 
