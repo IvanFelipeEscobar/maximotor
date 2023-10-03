cc81ad087c7f954544a186bfd1f429305161d89a
Ivan Escobar <ivnescbr1989@gmail.com>
2023-10-03 16:41:33 -0400

    user route logic

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,5 +1,6 @@
 import { user } from "../models/user";
 import {Request, Response} from 'express'
+import { signToken } from "../utils/auth";
 
 module.exports = {
     async getUser(req: Request , res: Response){
@@ -18,13 +19,38 @@ module.exports = {
             return res.status(500).json({ message: 'server error, skill issue likely ;)' });
         }
     },
-    async createUser(){
+    async createUser(req: Request, res: Response){
+        try {
+            const newUser = await user.create(req.body)
+            if(!newUser) return res.json(400).json({message: `something went wrong!!!`});
+            const token = signToken(newUser)
+            res.json({token, newUser})
+            
+        } catch (error) {
+            console.error(error)
+            return res.status(500).json({ message: 'server error, review uesr info )' })
+        }
 
     },
-    async editUser() { 
-
-    },
-    async deleteUser(){
-
+    async login(req: Request, res: Response) {
+        try {
+            const loggedUser = await user.findOne({
+                $or: [
+                    {username: req.body.username},
+                    {email: req.body.email}
+                ]
+            })
+            if(!loggedUser) return res.json(400).json({message: `something went wrong!!!`})
+            //verify password LOGIC!?!?!??!?!??!?!?!?!!??!?!!?
+        } catch (error) {
+            
+        }
     }
+
+    // async editUser() { 
+
+    // },
+    // async deleteUser(){
+
+    // }
 }
