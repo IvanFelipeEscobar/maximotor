168c3572c9721c0e7f627a6f840666f6aeeafa09
Ivan Escobar <ivnescbr1989@gmail.com>
2023-10-18 18:49:03 -0400

    react-roter-dom setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,15 @@
 
+import { Outlet } from 'react-router-dom'
 import './App.css'
 import Footer from './components/Footer'
-import Login from './components/Login'
 import Navbar from './components/Navbar'
-import Signup from './components/Signup'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Hero from './pages/Hero'
-import Services from './pages/Services'
 
 function App() {
 
   return (
     <>
      <Navbar/>
-     <Hero />
-     <Services/>
-     <About />
-     <Contact/>
-     <hr/>
-     <Login/>
-     <Signup/>
+     <Outlet />
      <Footer />
      
     </>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Landing from "./pages/Landing.tsx";
+import Signup from "./components/Signup.tsx";
+import Login from "./components/Login.tsx";
+import Services from "./pages/Services.tsx";
+import Contact from "./pages/Contact.tsx";
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {index: true, element: <Landing/>},
+      {path: '/signup', element: <Signup/>}, //todo: signup/login need to be styled
+      {path: '/login', element: <Login/>},
+      {path: '/appointments', element: <Services/>}, //todo: make appointments page
+      {path: '/user', element: <Contact/>}
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router}/>
     </ChakraProvider>
   </React.StrictMode>
 );
