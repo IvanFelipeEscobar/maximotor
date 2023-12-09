735f1b0da0c43cc7bfaa2307e0744f083dbd16a7
Ivan Escobar <ivnescbr1989@gmail.com>
2023-12-09 11:06:38 -0500

    repair history crud logic stuffs

--- server/src/controllers/repairs.ts
+++ server/src/controllers/repairs.ts
@@ -13,7 +13,50 @@ export const addRepair = async (req: Request, res: Response) => {
       { $push: { repairs: newRepair } },
       { new: true, runValidators: true }
     );
-    if(!addRepairToVehicle)return res.status(404).json({message: 'unable to add repair to vehicle'})
-    
-  } catch (error) {}
+    if (!addRepairToVehicle)
+      return res
+        .status(404)
+        .json({ message: "unable to add repair to vehicle" });
+    res.status(200).json(addRepairToVehicle);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "server error - :(" });
+  }
+};
+export const editRepair = async (req: Request, res: Response) => {
+  try {
+    const { repairId } = req.params;
+    const alteredRepair = await repairs.findOneAndUpdate(
+      { _id: repairId },
+      { $set: req.body },
+      { new: true, runValidators: true }
+    );
+    if (!alteredRepair)
+      return res
+        .status(404)
+        .json({ message: "can not edit repair check input" });
+    res.status(200).json(alteredRepair);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "server error - >:|" });
+  }
+};
+
+export const deleteRepair = async (req: Request, res: Response) => {
+  try {
+    const { vehicleId, repairId } = req.params;
+    const removedRepair = await repairs.findOneAndDelete({ _id: repairId });
+    if (!removedRepair)
+      return res.status(404).json({ message: "can't find that repair" });
+    const removeRepairFromVeh = await vehicle.findOneAndUpdate(
+        {_id: vehicleId},
+        {$pull: {repairs: repairId}},
+        {new: true}
+        );
+    if(!removeRepairFromVeh)return res.status(404).json({message: 'vehicle not found - '})
+    res.status(200)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "server error - :/" });
+  }
 };
