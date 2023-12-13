f566585d21ec9635d3eac66f78280e250a2e80f7
Ivan Escobar <ivnescbr1989@gmail.com>
2023-12-13 13:29:17 -0500

    add user controller, routing

--- server/src/controllers/user-info.ts
+++ server/src/controllers/user-info.ts
@@ -1,10 +1,39 @@
 import { user } from "../models/user";
 import { userInfo } from "../models/user-info";
-import {Request, Response} from 'express'
+import { Request, Response } from "express";
 import { signToken } from "../utils/auth";
 
-
-
-   export const addUserInfo =  async (req: Request, res: Response) => {
-
-    }
+export const addUserInfo = async (req: Request, res: Response) => {
+  try {
+    const newInfo = await userInfo.create(req.body);
+    if (!newInfo)
+      return res
+        .status(400)
+        .json({ message: "user info missing, check required fields" });
+    const addInfo = await user.findOneAndUpdate(
+      { email: req.user.email },
+      { $set: { userInformation: newInfo } },
+      { new: true, runValidators: true }
+    );
+    if(!addInfo)return res.status(404).json({message: 'unable to add info to user, check if user exists or something, idk'})
+    res.status(200).json(addInfo) // make sure you need to send this data here!!!
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: `whoops, Server issue. get it together!` });
+  }
+};
+export const editUserInfo = async (req: Request, res: Response) => {
+   try {
+      const  {userInfoId} = req.params
+      const editedInfo = await userInfo.findOneAndUpdate(
+         {_id: userInfoId},
+         {$set: req.body},
+         {new: true, runValidators: true}
+      )
+      if(!editedInfo)return res.status(400).json({message: 'can edit info, chenck input'})
+      res.status(200).json(editedInfo)   // make sure you need to send this data here!!!
+   } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: `whoops, Server issue. get it together!` });
+   }
+}
--- server/src/routes/api/user/index.ts
+++ server/src/routes/api/user/index.ts
@@ -5,9 +5,11 @@ import {
     login,
 } from '../../../controllers/user'
 import vehicleRoute from './vehicles'
+import userInfoRoute from './info'
 const router = express.Router()
 
 router.use(`/vehicles`, vehicleRoute)
+router.use('/user-info', userInfoRoute)
 // -------> /api/users...
 router.route(`/signup`).post(createUser)  
 router.route('/login').post(login)
