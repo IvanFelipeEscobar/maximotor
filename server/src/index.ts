dc2c43fe018e058db5522f5d94a2e5e44f6bdf40
Ivan Escobar <ivnescbr1989@gmail.com>
2023-12-21 18:07:44 -0500

    cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:5173'
+  origin: ['http://localhost:5173', 'https://maximotor.vercel.app/']
 }))
 app.use(router);
 
