bf288499fc8bee3147fda972b45be08debc66c2e
Ivan Escobar <ivnescbr1989@gmail.com>
2023-12-21 10:59:27 -0500

    ux/ui: changed nav postion to fixed

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -35,7 +35,7 @@ export default function Navbar() {
 
   return (
     <>
-      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4} pos={"fixed"} w={'full'} zIndex={99}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
           <IconButton
             size={"md"}
