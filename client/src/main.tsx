70b592d4df7cba8bfcd9b0abd19266b45529034b
Ivan Escobar <ivnescbr1989@gmail.com>
2024-01-23 08:43:09 -0500

    vercel.json for routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,18 +8,18 @@ import Signup from "./components/Signup.tsx";
 import Login from "./components/Login.tsx";
 import Services from "./components/Services.tsx";
 import UserDashboard from "./pages/User-Dashboard.tsx";
-import NewUser from "./components/NewUser.tsx";
+// import NewUser from "./components/NewUser.tsx";
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     children: [
       {index: true, element: <Landing/>},
-      {path: '/signup', element: <Signup/>}, //todo: signup/login need to be styled
+      {path: '/signup', element: <Signup/>}, 
       {path: '/login', element: <Login/>},
       {path: '/appointments', element: <Services/>}, //todo: make appointments page
-      {path: '/user-dashboard', element: <UserDashboard/>},
-      {path: '/new-user', element: <NewUser/>}
+      {path: '/user-dashboard', element: <UserDashboard/>},//to do: user dash
+      // {path: '/new-user', element: <NewUser/>}
 
     ]
   }
