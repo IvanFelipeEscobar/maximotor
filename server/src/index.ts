53e6b71c8bef3a0845ba1a5fd6cc90658de242bb
Ivan Escobar <ivnescbr1989@gmail.com>
2024-02-08 09:35:58 -0500

    server optimize

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,15 +6,16 @@ import cors from 'cors';
 
 const app: Express = express();
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3001;
-
+//define environment enum
 enum Env {
   Development = 'development',
   Production = 'production'
 }
-//variable to handle enviornment awareness
+//variable to handle environment awareness
 const env = process.env.NODE_ENV === Env.Production ? Env.Production : Env.Development
-
+//middleware for parsing json
 app.use(express.json());
+//middleware for parsing url encoded data
 app.use(express.urlencoded({ extended: true }));
 
 // Define CORS options
@@ -25,13 +26,13 @@ const corsOptions: cors.CorsOptions = {
 
 // Enable CORS middleware with options
 app.use(cors(corsOptions));
-
+//serve static files only in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../client/dist")));
 }
-
+//routes are defined in separate folder to keep concerns separated
 app.use(router);
-
+//database connection
 db.once(`open`, () => {
   app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 });
