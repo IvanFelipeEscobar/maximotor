960d71b7944bfc89488da24aa3551bb271a0cecd
Ivan Escobar <ivnescbr1989@gmail.com>
2024-03-08 13:32:57 -0500

    nav bar feels done now, finally

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -12,68 +12,80 @@ import {
   MenuItem,
   useColorMode,
   Text,
-  Stack,
+  MenuDivider,
 } from "@chakra-ui/react";
 import { HiOutlineUserCircle } from "react-icons/hi2";
 import { Auth } from "../utils/auth";
 
 import { BsSun, BsMoonStarsFill } from "react-icons/bs";
 
-// const Links = [
-//   {a: "About us", b: "/#about"},
-//   {a:"Contact us", b:'#contact'},
-//   {a:"Services", b:'#services'}
-// ]
-
+const navLinks = [
+  { a: "About us", b: "#about" },
+  { a: "Contact us", b: "#contact" },
+  { a: "Services", b: "#services" },
+];
+const isHomePage = window.location.pathname === "/";
 export default function Navbar() {
   const { colorMode, toggleColorMode } = useColorMode();
-  // const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
       <Box
         bg={useColorModeValue("gray.100", "gray.900")}
-        py={[5, 1]}
+        py={1}
         px={[2, 5]}
         pos={"fixed"}
-        top={'0'}
+        top={"0"}
         w={"full"}
         zIndex={99}
       >
-        <Flex h={16} alignItems={"center"} justifyContent={['space-between']} >
-            <Box>
-              <Link href={"/"}>
-                <Text
-                  as={"b"}
-                  fontSize={{ base: "xl", md: "2xl" }}
-                  letterSpacing={[2, 7]}
-                  fontWeight={'extrabold'}
-                  color={'red.500'}
-                  bg={useColorModeValue('rgb(41, 63, 137)', 'rgba(71,90,127,0.6)')}
-                  p={2}
-                  rounded={'lg'}
-                  shadow={'md'}>
-                  Maximotor
-                </Text>
-              </Link>
-            </Box>
+        <Flex h={16} alignItems={"center"} justifyContent={["space-between"]}>
+          <Box>
+            <Link href={"/"}>
+              <Text
+                as={"b"}
+                fontSize={{ base: "xl", md: "2xl" }}
+                letterSpacing={[2, 7]}
+                fontWeight={"extrabold"}
+                color={"red.500"}
+                bg={useColorModeValue(
+                  "rgb(41, 63, 137)",
+                  "rgba(71,90,127,0.6)"
+                )}
+                p={2}
+                rounded={"lg"}
+                shadow={"md"}
+              >
+                Maximotor
+              </Text>
+            </Link>
+          </Box>
 
-
-          <Flex alignItems={'center'}>
-
-          <Stack direction={'row'} spacing={2}>
+          <Flex alignItems={"center"}>
+            <Button
+              marginLeft={[28, 14]}
+              bg={useColorModeValue("gray.400", "inherit")}
+              aria-label="Toggle Color Mode"
+              size={"sm"}
+              onClick={toggleColorMode}
+              _focus={{ boxShadow: "none" }}
+              w="fit-content"
+            >
+              {colorMode === "light" ? <BsMoonStarsFill /> : <BsSun />}
+            </Button>
             <Menu>
               <MenuButton
+                aria-label="sign in/log out"
                 as={Button}
                 variant={"solid"}
                 colorScheme={"red"}
                 bg={"red.400"}
                 _hover={{ bg: "red.500" }}
                 size={"sm"}
-                leftIcon={<HiOutlineUserCircle />}
-                display={['none', 'inherit']}
+                visibility={["inherit", "collapse"]}
+                marginLeft={3}
               >
-                {Auth.isLoggedIn() ? "Log out" : "Sign up/Login"}
+                <HiOutlineUserCircle />
               </MenuButton>
               <MenuButton
                 as={Button}
@@ -82,32 +94,42 @@ export default function Navbar() {
                 bg={"red.400"}
                 _hover={{ bg: "red.500" }}
                 size={"sm"}
-                display={['inherit','none']}
-              ><HiOutlineUserCircle /></MenuButton>
-              <MenuList>
+                leftIcon={<HiOutlineUserCircle />}
+                visibility={["collapse", "inherit"]}
+              >
+                {Auth.isLoggedIn() ? "Log out" : "Sign up/Login"}
+              </MenuButton>
+              <MenuList alignItems={"center"}>
+                {isHomePage && navLinks.map((link) => (
+                  <MenuItem as={"a"} href={link.b} key={link.a}>
+                    {link.a}
+                  </MenuItem>
+                ))}
+<MenuDivider/>
+                <MenuItem as={"a"} href="/appointments">
+                  Make an appointment
+                </MenuItem>
+                <MenuItem as={"a"} href="/user-dashboard">
+                  User dashboard
+                </MenuItem>
+                <MenuDivider />
+
                 {Auth.isLoggedIn() ? (
-                  <MenuItem as={'button'} onClick={() => Auth.logout()}>Logout
+                  <MenuItem as={"button"} onClick={() => Auth.logout()}>
+                    Logout
                   </MenuItem>
                 ) : (
                   <>
-                      <MenuItem as={'a'} href="/login">Login</MenuItem>
-                      <MenuItem as={'a'} href="/signup">Sign up</MenuItem>
+                    <MenuItem as={"a"} href="/login">
+                      Login
+                    </MenuItem>
+                    <MenuItem as={"a"} href="/signup">
+                      Sign up
+                    </MenuItem>
                   </>
                 )}
               </MenuList>
             </Menu>
-
-            <Button
-            bg={useColorModeValue( "gray.400", "inherit")}
-              aria-label="Toggle Color Mode"
-              size={'sm'}
-              onClick={toggleColorMode}
-              _focus={{ boxShadow: "none" }}
-              w="fit-content"
-            >
-              {colorMode === "light" ? <BsMoonStarsFill /> : <BsSun />}
-            </Button>
-            </Stack>
           </Flex>
         </Flex>
       </Box>
