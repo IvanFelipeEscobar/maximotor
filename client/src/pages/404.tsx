84da7f8aea23c9c43bfdf0d9d18a0e8ec0e04600
Ivan Escobar <ivnescbr1989@gmail.com>
2024-03-14 15:31:09 -0400

    404 page style

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -1,33 +1,33 @@
-'use client'
-
-import { Box, Heading, Text, Button } from '@chakra-ui/react'
+import { Box, Heading, Text, Button } from "@chakra-ui/react";
 
 export default function NotFound() {
   return (
-    <Box textAlign="center" py={10} px={6}>
+    <Box textAlign="center" py={10} px={6} bg={"rgb(41, 63, 137)"} h={'100vh'} m={'auto'}>
       <Heading
         display="inline-block"
         as="h2"
         size="2xl"
-        bgGradient="linear(to-r, teal.400, teal.600)"
-        backgroundClip="text">
+        bgGradient="linear(to-r, red.400, red.600)"
+        backgroundClip="text"
+      >
         404
       </Heading>
-      <Text fontSize="18px" mt={3} mb={2}>
+      <Text fontSize="18px" mt={3} mb={2} color={"aliceblue"}>
         Page Not Found
       </Text>
-      <Text color={'gray.500'} mb={6}>
+      <Text color={"gray.300"} mb={6}>
         The page you&apos;re looking for does not seem to exist
       </Text>
 
       <Button
-        colorScheme="teal"
-        bgGradient="linear(to-r, teal.400, teal.500, teal.600)"
+        colorScheme="red"
+        bgGradient="linear(to-r, red.400, red.500, red.600)"
         color="white"
         variant="solid"
-        onClick={()=> window.location.assign('/')}>
+        onClick={() => window.location.assign("/")}
+      >
         Go to Home
       </Button>
     </Box>
-  )
+  );
 }
